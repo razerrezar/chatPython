2f81a80009c140187b9301721356696cb1e7b00e
razerrezar <razerrezar@gmx.fr>
2016-04-29 13:08:52 +0200

    Création des widgets
    Créations des principaux widgets.

--- main.py
+++ main.py
@@ -2,8 +2,26 @@
 
 import Tkinter
 
+def envoyer():
+    pass
+
+def actualiser():
+    pass
+
 if __name__=="__main__":
     mainFen = Tkinter.Tk()
     mainFen.title("Chat")
+    
+    buttonActualiser = Tkinter.Button(mainFen, text = "Actualiser", command = actualiser)
+    buttonActualiser.pack(side = "top")
+    
+    labelTexte = Tkinter.Label(mainFen)
+    labelTexte.pack(side = "top")
 
+    entryMessage = Tkinter.Entry(mainFen)
+    entryMessage.pack(side = "left")
+
+    buttonSubmit = Tkinter.Button(mainFen, text = "envoyer", command = envoyer)
+    buttonSubmit.pack(side = "right")
+    
     mainFen.mainloop()
